8b8d78e7cee1f166a2d6acca57394c126156e650
yaelle chaudy <yaelle.chaudy@uws.ac.uk>
2021-11-12 14:02:03 +0100

    Added event syncing

--- mini-microservices app/client/src/CommentList.js
+++ mini-microservices app/client/src/CommentList.js
@@ -2,7 +2,23 @@ import React from 'react';
 
 export default ({ comments }) => {
     const renderedComments = comments.map(comment => {
-      return <li id={comment.id}>{ comment.content }</li>;
+      let content;
+
+      switch (comment.status) {
+        case 'approved' :
+          content = comment.content;
+          break;
+        case 'pending' :
+          content = 'Comment awaiting moderation';
+          break;
+        case 'rejected' :
+          content = 'Comment rejected';
+          break;
+        default:
+          content = '';
+      }
+
+      return <li id={comment.id}>{ content }</li>;
     });
 
     return (
