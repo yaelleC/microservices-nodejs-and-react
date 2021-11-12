0e26c7812090846a76719bddc6d7ff6bc65f0499
yaelle chaudy <yaelle.chaudy@uws.ac.uk>
2021-11-12 13:23:47 +0100

    Added comment moderation

--- mini-microservices app/comments/index.js
+++ mini-microservices app/comments/index.js
@@ -20,20 +20,50 @@ app.post('/posts/:id/comments', async (req, res) => {
 
     const comments = commentsByPostId[req.params.id] || [];
 
-    comments.push({ id: commentId, content });
+    comments.push({ id: commentId, content, status: 'pending' });
 
     commentsByPostId[req.params.id] = comments;
 
     await axios.post('http://localhost:4005/events', {
        type: 'CommentCreated',
-       data: { id: commentId, content, postId: req.params.id }
+       data: { 
+           id: commentId, 
+           content, 
+           status: 'pending', 
+           postId: req.params.id 
+        }
     });
 
     res.status(201).send(comments);
 });
 
-app.post('/events', (req, res) => {
+app.post('/events', async (req, res) => {
     console.log('Received Event', req.body.type);
+
+    const { type, data } = req.body;
+
+    if (type === 'CommentModerated') {
+        const { postId, id, status, content } = data;
+        const comments = commentsByPostId[postId];
+
+        const comment = comments.find(comment => {
+            return comment.id === id;
+        });
+        comment.status = status;
+
+        await axios.post('http://localhost:4005/events', {
+            type: 'CommentUpdated',
+            data: {
+                id,
+                content,
+                postId,
+                status
+            }
+        }).catch((err) => {
+            console.log(err.message);
+        });
+    }
+
     res.send({});
 });
 
