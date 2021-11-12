6f1c20a6dfac33dd06dc26757b6edc802cf4bc0b
yaelle chaudy <yaelle.chaudy@uws.ac.uk>
2021-11-12 12:04:04 +0100

    Updated comments and Posts to send event to bus

--- mini-microservices app/comments/index.js
+++ mini-microservices app/comments/index.js
@@ -32,6 +32,11 @@ app.post('/posts/:id/comments', async (req, res) => {
     res.status(201).send(comments);
 });
 
+app.post('/events', (req, res) => {
+    console.log('Received Event', req.body.type);
+    res.send({});
+});
+
 app.listen(4001, () => {
     console.log('listening on 4001');
 });
